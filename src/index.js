ff08cfc31abe9822113c2b9d9de6d696d2c5ac46
Gabriel Aderaldo <gabriel_aderaldo@edu.unifor.br>
2020-06-12 05:12:18 -0300

    Adição no projeto, as validações de acesso de usuario pelo token e pelo ID

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ app.use(bodyParser.urlencoded({extended: false}))
 
 require('./controllers/playlistController')(app);
 require('./controllers/songsController')(app);
-require('./controllers/userController')(app)
+require('./controllers/userController')(app);
+require("./controllers/projectController")(app);
 
 app.listen(portas,function(){
 console.log("O servidor está rondando na porta: ",portas);
